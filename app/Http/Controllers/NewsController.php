b1c105d15a7bad67cf1626884d022f9cc08ab9ac
ShanXinxin <yison@dripower.com>
2016-01-30 23:03:05 +0800

    update

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -98,9 +98,9 @@ class NewsController extends Controller {
    function log() {
      set_time_limit(0);
      if(isset($_GET['f']) && $_GET['f'] == 'err') {
-       $output = shell_exec("cat ~/aa/err.log");
+       $output = shell_exec("cat aa/err.log");
      } else {
-       $output = shell_exec("cat ~/aa/out.log");
+       $output = shell_exec("cat aa/out.log");
      }
      print_r($output);
    }
