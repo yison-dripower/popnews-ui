52d78da2effd6fcdc09f2278e8e75d3cadfeb79f
Yison <yison@dripower.com>
2017-03-24 11:40:17 +0800

    update

--- aa/aa.js
+++ aa/aa.js
@@ -1,3 +1,5 @@
+require('phantomjs-polyfill')
+
 var arguments = process.argv;
 var url = arguments[2];
 var news = arguments[3];
@@ -6,15 +8,26 @@ var link = arguments[5];
 var phantom    = require("phantom");
 var sleep = require("sleep");
 
-phantom.create(function (ph) {
-  ph.createPage(function (page) {
-    page.open(url, function (status) {
+console.log('url:' + url)
+console.log('news:' + news)
+console.log('title:' + title)
+console.log('link:' + link)
+
+var USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36';
+
+phantom.create(['--ignore-ssl-errors=yes', '--load-images=no'])
+  .then(instance => {
+    phInstance = instance
+    return instance.createPage()
+  })
+  .then(page => {
+    page.open(url).then(function (status) {
       var rule = {
         news: news,
         title: title,
         link: link
       }
-      sleep.sleep(3);
+      sleep.sleep(3)
       page.evaluate(function (rule) {
         var newsList = document.querySelectorAll(rule.news);
         var list = [];
@@ -24,8 +37,8 @@ phantom.create(function (ph) {
         for(i=0; i < newsList.length; i++) {
           var item = newsList[i];
           if(typeof item == 'object') {
-            var title = item.querySelector(rule.title);
-            var link = item.querySelector(rule.link);
+            var title = rule.title == '.' ? item : item.querySelector(rule.title);
+            var link = rule.link == '.' ? item : item.querySelector(rule.link);
             var news = {};
             news['title'] = title.innerText;
             if(link != undefined && link != null) {
@@ -35,18 +48,19 @@ phantom.create(function (ph) {
           list.push(news);
         }
         return list ;
-      }, function (result) {
+      }, rule).then(function(result){
         if(result === false  || result == null || result.length < 1) {
           console.log('没有找到新闻');
         } else if(result[0]['title'] == undefined) {
           console.log('没有找到标题');
-        } else if(result[0]['link'] == undefined) {
-          console.log('没有找到新闻链接');
         }else {
           console.log('测试成功！');
         }
-        ph.exit();
-      }, rule);
-    });
+        phInstance.exit()
+      });
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    phInstance.exit()
   });
-});
